ca7bf877e56a02d252a1ef7d03e36648dc508624
asano <c0a2100605@edu.teu.ac.jp>
2022-06-21 16:46:23 +0900

    とくになし

--- ex02/calc.py
+++ ex02/calc.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     entry.grid(row=0, column=0, columnspan=4)
 
     r, c = 1, 0 #r:行番号　c:列番号
-    for i, num in enumerate(["**", "**0.5", "", "/", 7, 8, 9, "*", 4, 5, 6, "-", 1, 2, 3, "+", "*-1", 0, ".", "="]):
+    for i, num in enumerate(["**", "**0.5", "", "/", 7, 8, 9, "*", 4, 5, 6, "-", 1, 2, 3, "+", "*-1", 0, ".", "="]): #表示させる文字
         btn = tk.Button(root,
                         text=f"{num}",
                         width=4,
