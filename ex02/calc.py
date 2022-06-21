d0a31b9fe1b68e2423023eeb1e65f642f2d17d3e
asano <c0a2100605@edu.teu.ac.jp>
2022-06-21 15:56:40 +0900

    追加1

--- ex02/calc.py
+++ ex02/calc.py
@@ -19,10 +19,10 @@ if __name__ == "__main__":
     root.title("電卓")
 
     entry = tk.Entry(root, justify="right", width=10, font=("Times New Roman", 40))
-    entry.grid(row=0, column=0, columnspan=3)
+    entry.grid(row=0, column=0, columnspan=4)
 
     r, c = 1, 0 #r:行番号　c:列番号
-    for i, num in enumerate([9, 8, 7, 6, 5, 4, 3, 2, 1, 0, "+", "="]):
+    for i, num in enumerate([7, 8, 9, "*", 4, 5, 6, "-", 1, 2, 3, "+", "*-1", 0, ".", "="]):
         btn = tk.Button(root,
                         text=f"{num}",
                         width=4,
@@ -33,7 +33,7 @@ if __name__ == "__main__":
         btn.grid(row=r, column=c)
 
         c += 1
-        if (i+1)%3 == 0:
+        if (i+1)%4 == 0:
             r += 1
             c = 0
 
