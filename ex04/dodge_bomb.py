3c3a815a55bc49df064229b2a972984538a71b10
asano <c0a2100605@edu.teu.ac.jp>
2022-07-05 14:24:35 +0900

    練習３，４

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -9,15 +9,17 @@ def main():
     screen_sfc = pg.display.set_mode((1600, 900)) #Surface
     screen_rect = screen_sfc.get_rect() #Rect
     bgimg_sfc = pg.image.load("fig/pg_bg.jpg") #Surface
-    bgimg_rect = bgimg_sfc.get_rect() #Rect
-    screen_sfc.blit(bgimg_sfc, bgimg_rect)
+    bgimg_rct = bgimg_sfc.get_rect() #Rect
+    screen_sfc.blit(bgimg_sfc, bgimg_rct)
 
-    #pg.display.update()
-
-    clock.tick(0.5)
+    kkimg_sfc = pg.image.load("fig/6.png") #Surface
+    kkimg_sfc = pg.transform.rotozoom(kkimg_sfc, 0, 2.0) #Surface
+    kkimg_rct = kkimg_sfc.get_rect() #Rect
+    kkimg_rct.center = 900, 400
 
     while True:
-        screen_sfc.blit(bgimg_sfc, bgimg_rect)
+        screen_sfc.blit(bgimg_sfc, bgimg_rct)
+        screen_sfc.blit(kkimg_sfc, kkimg_rct)
 
         #練習２
         for event in pg.event.get():
