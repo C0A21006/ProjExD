b577416357e2e46a88494f8d294dabc10995a8e3
asano <c0a2100605@edu.teu.ac.jp>
2022-07-05 15:40:47 +0900

    練習6,7

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -21,10 +21,13 @@ def main():
 
     #練習５
     bmimg_sfc = pg.Surface((20, 20)) #Surface
+    bmimg_sfc.set_colorkey((0, 0, 0))
     pg.draw.circle(bmimg_sfc, (255, 0, 0), (10, 10), 10)
     bmimg_rct = bmimg_sfc.get_rect() #Rect
     bmimg_rct.centerx = random.randint(0, screen_rct.width)
     bmimg_rct.centery = random.randint(0, screen_rct.height)
+    vx = 1
+    vy = 1
 
     while True:
         screen_sfc.blit(bgimg_sfc, bgimg_rct)
@@ -40,13 +43,38 @@ def main():
         if key_states[pg.K_DOWN]  == True:kkimg_rct.centery += 1
         if key_states[pg.K_LEFT]  == True:kkimg_rct.centerx -= 1
         if key_states[pg.K_RIGHT] == True:kkimg_rct.centerx += 1
+        #練習７
+        if check_bound(kkimg_rct, screen_rct) != (1, 1): #領域外だったら
+            if key_states[pg.K_UP]    == True:kkimg_rct.centery += 1
+            if key_states[pg.K_DOWN]  == True:kkimg_rct.centery -= 1
+            if key_states[pg.K_LEFT]  == True:kkimg_rct.centerx += 1
+            if key_states[pg.K_RIGHT] == True:kkimg_rct.centerx -= 1
         screen_sfc.blit(kkimg_sfc, kkimg_rct)
+
+        #練習６
+        bmimg_rct.move_ip(vx, vy)
 
         #練習５
         screen_sfc.blit(bmimg_sfc, bmimg_rct)
-        
+
+        #練習７
+        yoko, tate = check_bound(bmimg_rct, screen_rct)
+        vx *= yoko
+        vy *= tate
+
         pg.display.update()
         clock.tick(1000)
+
+#練習７
+def check_bound(rct, scr_rct):
+    '''
+    [1]rct:こうかとん or 爆弾のRect
+    [2]scr_rct:スクリーンのRect
+    '''
+    yoko, tate = +1, +1
+    if rct.left < scr_rct.left or scr_rct.right  < rct.right:yoko  = -1
+    if rct.top  < scr_rct.top  or scr_rct.bottom < rct.bottom:tate = -1
+    return yoko, tate
 
 if __name__ == "__main__":
     pg.init()
