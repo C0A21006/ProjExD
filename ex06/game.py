4f98e12f199611969921938f939c0a94834fa817
asano <c0a2100605@edu.teu.ac.jp>
2022-07-19 18:03:52 +0900

    個人成果物ver01

--- ex06/game.py
+++ ex06/game.py
@@ -1,132 +1,104 @@
 import pygame as pg
 import sys
 import random
-import numpy as np
-max_bm = 0
-bg_x = 0
-maxspeed = 3
-t = 0
-#スクリーン用クラス
+
 class Screen:
-    def __init__(self, title, wh: tuple, img):
+    def __init__(self, title, wh, image):
         pg.display.set_caption(title)
         self.sfc = pg.display.set_mode(wh)
-        self.rect = self.sfc.get_rect()
-        self.bg_sfc = pg.image.load(img).convert()
-        self.bg_rect = self.bg_sfc.get_rect()
+        self.rct = self.sfc.get_rect()
+        self.bgi_sfc = pg.image.load(image)    
+        self.bgi_rct = self.bgi_sfc.get_rect()
+        self.bg_x = 0
 
-    def blit(self,bg_x):
-        self.sfc.blit(self.bg_sfc,[bg_x-1600,0])
-        self.sfc.blit(self.bg_sfc,[bg_x,0])
+    def blit(self):
+        self.sfc.blit(self.bgi_sfc, [self.bg_x - self.rct.width, 0])  #背景を描画
+        self.sfc.blit(self.bgi_sfc, [self.bg_x, 0])                   #ずらした分も描画
+        self.bg_x = (self.bg_x - 5) % self.rct.width                       #背景を動かす
 
-#こうかとん用クラス
 class Bird:
-    def __init__(self, img, size: float, xy: tuple):
-        self.sfc = pg.image.load(img)
-        self.sfc = pg.transform.rotozoom(self.sfc, 0, size)
-        self.rect = self.sfc.get_rect()
-        self.rect.center = xy
-        self.vey = 0
-        self.jump = False
+    def __init__(self, image: str, size: float, x, y):
+        self.sfc = pg.image.load(image)    
+        self.sfc = pg.transform.rotozoom(self.sfc, 0, size)  
+        self.rct = self.sfc.get_rect()     
+        #self.rct.center = xy
+        self.rct.x = x
+        self.rct.y = y
 
-    def blit(self, screen: Screen):
-        screen.sfc.blit(self.sfc, self.rect)
-
-    def update(self, screen: Screen):
-        key = pg.key.get_pressed()
-        tori_speed = 1
-        if key[pg.K_UP]:
-            self.rect.centery -= tori_speed
-        if key[pg.K_DOWN]:
-            self.rect.centery += tori_speed
-        if key[pg.K_LEFT]:
-            self.rect.centerx -= tori_speed
-        if key[pg.K_RIGHT]:
-            self.rect.centerx += tori_speed
-        if key[pg.K_SPACE] and self.jump == False:
-            self.jump = True
-            self.rect.centery-= 5
-        if self.jump == True:
-            self.rect.centery +=1
-        
-        if check_bound(self.rect, screen.rect) != (1, 1):
-            if key[pg.K_UP]:
-                self.rect.centery += tori_speed
-            if key[pg.K_DOWN]:
-                self.rect.centery -= tori_speed
-            if key[pg.K_LEFT]:
-                self.rect.centerx += tori_speed
-            if key[pg.K_RIGHT]:
-                self.rect.centerx -= tori_speed
-        self.blit(screen)
+        self.vel_y = 35                    #こうかとんの加速度を設定
 
 
-#爆弾用クラス
+    def blit(self, scr: Screen):
+        scr.sfc.blit(self.sfc, self.rct)
+
+    def update(self, scr: Screen):
+        key_states = pg.key.get_pressed()
+
+        if self.rct.y+self.vel_y >= 500:   #y座標と加速度を合わせた値が500を超えたら
+            self.rct.y = 500               #y座標を500に固定する
+            if key_states[pg.K_SPACE]:     #スペースキーが押されたら
+                self.vel_y = -25           #加速度を-25に設定する
+
+        self.vel_y += 1                    #加速度を毎tick1ずつ増やす
+
+        if self.vel_y > 15:                #加速度が15を超えたら
+            self.vel_y = 15                #15に固定する
+
+        self.rct.y += self.vel_y           #y座標に加速度を足し合わせる
+
+        self.blit(scr)
+
+
 class Bomb:
-    def __init__(self, color: tuple, rad: int, screen: Screen):
-        self.sfc = pg.Surface((rad * 2 , rad * 2))
-        self.sfc.set_colorkey((0, 0, 0))
-        for i in range(4):
-            pg.draw.circle(self.sfc, (color[0] - i * color[0] // 4, color[1] - i * color[1] // 4, color[2] - i * color[2] // 4), (rad, rad), rad - i * (rad // 4 + 1))
-        self.rect = self.sfc.get_rect()
-        self.rect.centerx = 1600
-        self.rect.centery = random.randint(rad * 2, screen.rect.height - rad * 2)
-        self.vx = np.random.choice([-1, 1])
+    def __init__(self, color, size, vxy, scr: Screen):
+        self.sfc = pg.Surface((2*size, 2*size))
+        self.sfc.set_colorkey((0, 0, 0)) 
+        pg.draw.circle(self.sfc, color, (size, size), size)
+        self.rct = self.sfc.get_rect()
+        self.rct.centerx = random.randint(0, scr.rct.width)
+        self.rct.centery = random.randint(0, scr.rct.height)
+        self.vx, self.vy = vxy 
 
+    def blit(self, scr: Screen):
+        scr.sfc.blit(self.sfc, self.rct)
 
-    def blit(self, screen: Screen):
-        screen.sfc.blit(self.sfc, self.rect)
-
-    def update(self, screen: Screen):
-        global maxspeed
-        global max_bm
-        max_bm = random.randint(1,5)
-        for i in range(max_bm):
-            self.rect.move_ip(-1,0)
-            maxspeed = 3
-            self.blit(screen)
+    def update(self, scr: Screen):
+        
+        self.rct.move_ip(self.vx, self.vy)
+        
+        yoko, tate = check_bound(self.rct, scr.rct)
+        self.vx *= yoko
+        self.vy *= tate   
+        
+        self.blit(scr)          
 
 
 def main():
-    global maxspeed
-    global bg_x
-    global t
-    #fpsのカウント開始
     clock = pg.time.Clock()
-    sc = Screen("負けるな！こうかとん", (1600, 900), "ex06/fig/pg_bg.jpg")
-    tori = Bird("ex06/fig/6.png", 2.0, (900, 400))
-    bomb = Bomb((255, 0, 0), 50, sc)
+    scr = Screen("逃げろ！こうかとん", (1600, 900), "fig/bg_sabaku.jpg")
+    kkt = Bird("fig/6.png", 2.5, 300, 500)
+    bkd = Bomb((255,0,0), 10, (+1,+1), scr)
 
-    #描画
     while True:
-        t += 1
-        bg_x = (bg_x-0.6)%1600
-        sc.blit(bg_x)
-
+        scr.blit()
 
         for event in pg.event.get():
-            if event.type == pg.QUIT:
-                return
+            if event.type == pg.QUIT: return
 
-        if t%2000 == 0:
-            bomb = Bomb((255, 0, 0), 50, sc)
-        tori.update(sc)
-        bomb.update(sc)
-
-        if tori.rect.colliderect(bomb.rect):
+        kkt.update(scr)
+        bkd.update(scr)
+        if kkt.rct.colliderect(bkd.rct):
             return
-        maxspeed += 0.001
 
         pg.display.update()
-        clock.tick(1000)
+        clock.tick(120)
 
-def check_bound(rect, sc_rect):
-    w, h = 1, 1
-    if rect.left < sc_rect.left or rect.right > sc_rect.right:
-        w = -1
-    if rect.top < sc_rect.top or rect.bottom > sc_rect.bottom:
-        h = -1
-    return w, h
+
+def check_bound(rct, scr_rct):
+    yoko, tate = +1, +1
+    if rct.left < scr_rct.left or scr_rct.right  < rct.right : yoko = -1
+    if rct.top  < scr_rct.top  or scr_rct.bottom < rct.bottom: tate = -1
+    return yoko, tate
 
 
 if __name__ == "__main__":
